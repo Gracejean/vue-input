25c48019efae0c7f6fa8e01ec42aecd9602fbd05
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2021-03-04 08:38:42 +0800

    money abbreviation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <div id="num" >
      <Money v-model="price"></Money>
       <p>Value: {{ price.replaceAll(',', '').split('').join("") }}</p>
+      <p>Amount: {{abbrevated}}</p>
     </div>
 </template>
 
@@ -17,11 +18,39 @@ export default {
         return {
             price: ''
         }
+    },
+     created() {
+    var a = 10000000;
+    var b = ['K', 'M'];
+    var numLen = a.toString().length;
+    var index = Math.ceil((numLen - 3) / 3);
+
+    if (numLen > 3) {
+    console.log(a/Math.pow(1000, index)+ b[index - 1])
+
+    }else{
+      console.log(a);
     }
+
+  },
+  computed:{
+    abbrevated: function(){
+      var num = this.price.replaceAll(',', '').split('').join('')
+      var b = ['K', 'M'];
+      var numLen = num.toString().length;
+      var index = Math.ceil((numLen - 3) / 3);
+
+      if (numLen > 3) {
+        return num/Math.pow(1000, index)+ b[index-1]
+
+      }else{
+        return num
+      }
+
+    }
+  }
+
 }
-
-  
-
 
 
 </script>
